2fe67e8bc5fa21004660699cff1d1dda1c95d8bc
Ryan Lee <rjlguitar@gmail.com>
2021-10-18 17:17:48 -0700

    finished callback problems

--- 02-callback-practice.js
+++ 02-callback-practice.js
@@ -8,9 +8,7 @@ console.log('1 ------------------------------');
 // TODO: The setTimeout function takes a callback. I've used a
 // regular function you should change it to an arrow function
 
-setTimeout(function () {
-  console.log('Example 1 - one second later');
-}, 1000);
+setTimeout(() => console.log('1 second later'), 1000);
 
 // A Callback is a function you pass as a parameter to
 // a function. These appear everywhere in javascript
@@ -18,6 +16,10 @@ setTimeout(function () {
 console.log('2 ------------------------------');
 
 // TODO: use setTimeout to print a message 3 seconds in the future:
+const reminder = () => {
+  console.log('3 seconds passed');
+};
+setTimeout(reminder, 3000);
 
 console.log('3 ------------------------------');
 
@@ -39,10 +41,17 @@ primes.forEach((n) => {
 
 // TODO: Use foreach to double each number in the numbers array
 // print the new value to the console.
+const double = (n) => console.log(n * 2);
+primes.forEach(double);
 
 // TODO: Use forEach to print only the even numbers to the console
 // You know a number is even if n % 2 === 0
-
+console.log('Even Numbers -----');
+primes.forEach((n) => {
+  if (n % 2 === 0) {
+    console.log(n);
+  }
+});
 // The forEach method calls the provided function with the following
 // parameters: item, index, array.
 // Read the documentation here:
@@ -63,7 +72,9 @@ primes.forEach((item, index, arr) => {
 
 // TODO: Use the index. Multiply each value by it's index in the array and
 // print the value to the console
-
+primes.forEach((item, index, arr) => {
+  console.log(item * index);
+});
 console.log('5 ------------------------------');
 
 /* TODO: ***********************************************
@@ -73,6 +84,10 @@ The callback in foreach receives a parameter that
 is an item from the array. In this case a name.
 */
 const names = ['Andy', 'Boba', 'Kris', 'Dana'];
+
+names.forEach((n) => {
+  console.log(n);
+});
 
 /* ***********************************************
 forEach provides a second parameter to it's
@@ -86,12 +101,14 @@ like this:
 4) Dana
 
 */
-
+names.forEach((item, index, arr) => {
+  console.log(index + 1 + ') ' + item);
+});
 /* ************************************************
 Use forEach and setTimeout to print each name
 with a delay of 1200ms.
 */
-
+names.forEach((name, n) => setTimeout(() => console.log(`${name}`), 1200 * n));
 console.log('6 ------------------------------');
 
 // These callbacks and forEach are really useful. You'll
@@ -104,5 +121,10 @@ console.log('6 ------------------------------');
 // callback as arguments. For each item in the array call the
 // callback and pass the item, the index, and the array into
 // the callback as arguments.
-
+const forEvery = (arr, call) => {
+  for (n in arr) {
+    call(arr[n]);
+  }
+};
+console.log(forEvery(names, (name) => console.log(`Name: ${name}`)));
 // NOTE! Do not use arr.forEach() to solve this problem!
