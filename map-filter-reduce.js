b2895210ba45899cfe897087853fa9726bf8f986
Ryan Lee <rjlguitar@gmail.com>
2021-10-20 17:07:01 -0700

    fixed problems

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -53,24 +53,20 @@ console.log(squares);
 // TODO: Map the users array into an array of name strings:
 // [{ name: "anatoly", ... }, {}, {}] -> ['anatoly', 'bobby', 'carlsen']
 console.log('4 -------');
-const usersList = users.map(function (users) {
-  return `${users.name}`;
-});
+const usersList = users.map((user) => user.name);
 console.log(usersList);
 
 // TODO: Map the array of users to an array of ratings:
 // [{ name: "anatoly", rating: 2848 }, {}, {}] -> [2848, 2785, 2882]
 console.log('5 -------');
-const ratingList = users.map(function (users) {
-  return `${users.rating}`;
-});
+const ratingList = users.map((user) => user.rating);
 console.log(ratingList);
 
 // TODO: Map the Users array into an array of string descriptions:
 // [{ name: "anatoly", rating: 2848 }, {}, {}] -> ['name: anatoly rating: 2848', ...]
 console.log('6 -------');
-const usersFullList = users.map(function (users) {
-  return `Name: ${users.name} Rating: ${users.rating}`;
+const usersFullList = users.map(function (user) {
+  return `Name: ${user.name} Rating: ${user.rating}`;
 });
 console.log(usersFullList);
 // --------------------------------------------------------------
@@ -95,9 +91,9 @@ console.log(primes10);
 // TODO: Filter the users array to a new array containing
 // Users with a rating greater than 2850
 console.log('9 -------');
-const rating2850 = users.filter(function (users) {
-  if (users.rating > 2850) {
-    return `Name: ${users.name} Rating: ${users.rating}`;
+const rating2850 = users.filter((user) => {
+  if (user.rating > 2850) {
+    return `Name: ${user.name} Rating: ${user.rating}`;
   }
 });
 console.log(rating2850);
@@ -132,11 +128,11 @@ console.log(sum);
 // as the name and value as the rating. For example:
 // [{ name: "anatoly", rating: 2848 }, {}, {}] -> { 'anatoly': 2848, 'bobby': 2785, 'carlsen': 2882 }
 console.log('12 -------');
-const userArray = users.reduce(
-  (acc, users) => `${acc} ${users.name} : ${users.rating},`,
-  ''
-);
-console.log(userArray);
+const usersObj = users.reduce((acc, name, rating) => {
+  acc[rating] = name;
+  return acc;
+}, {});
+console.log(usersObj);
 // ----------------------------------------------------------
 
 // Combine map filter and reduce!
@@ -161,12 +157,12 @@ console.log(primesLess10);
 // users.filter().map() -> ['anatoly rating: 2848', ...]
 console.log('14 -------');
 const players = users
-  .filter(function (users) {
+  .filter((users) => {
     if (users.rating > 2850) {
       return `Name: ${users.name} Rating: ${users.rating}`;
     }
   })
-  .map(function (users) {
+  .map((users) => {
     return `Name: ${users.name} Rating: ${users.rating}`;
   });
 console.log(players);
